c0dc12ba2b03e9e96f1d394a941c765926a7cbcd
CraigKnoblauch <craigknoblauch@gmail.com>
2018-09-29 19:44:54 -0700

    LED's aren't working properly

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,18 @@
 int col[4] = {12, 11, 10, 9};
 int row[2] = {6, 5};
 
-void setup() {
+Matrix matrix = Matrix( sizeof(row), row, sizeof(col), col );
 
+void setup() {
+    matrix.begin();
 }
 
-void reset() {
-
+void loop() {
+    // Loop through all the LED's
+    for(int i = 0; i < sizeof(row)/4; i++) {
+        for(int j = 0; j < sizeof(col)/4; j++) {
+            matrix.setLED(i, j);
+            delay(500);
+        }
+    }
 }
