e1ca30c7248b01e00313d79c028e4699413ec571
CraigKnoblauch <craigknoblauch@gmail.com>
2018-10-01 22:21:31 -0700

    reworking algorithm in getSelection; fixed a small error in setNumSelections

--- lib/TrimPot/TrimPot.cpp
+++ lib/TrimPot/TrimPot.cpp
@@ -19,18 +19,34 @@ int TrimPot::getSelection() {
     int val = analogRead(wiper);
 
     /**
-    * Assures that the correct position is idenitifed. For example, if there are
-    * 3 selection options, and the value of trimpot is 28, this would be the 0 selection.
-    * It follows that (28 + 1) / 3 = 0
+    * For some amount of selections N, the amount of possible values per selection,
+    * m, is equal to 1024/N (I've decided to take the floor for now until I figure out
+    * how to distribute the remainders).
+    * Given this arrangement, the values are distributed like so:
+    *
+    * 0 to m-1, m to 2m-1, ..., (N-1)m to Nm - 1
     */
-    int selection = (val + 1) / num_selections;
+
+    int m = 1024/num_selections;
+
+    // if none of the selections are matched, it's likely the remainders have accumulated
+    // such that the val is not in the range, thus it will not be set in the loop, but should
+    // still be the last selection
+    int selection = num_selections - 1;
+
+    for(int i = 1; i <= num_selections; i++) {
+        if( val >= ( (i-1)*m ) && val <= ( i*m - 1 ) ) {
+            selection = i - 1;
+            break;
+        }
+    }
 
     return selection;
 }
 
 int TrimPot::setNumSelections(int n) {
     int status = 0;
-    if( n <= 1024 && n >= 0 ) {
+    if( n <= 1024 && n >= 1 ) {
         num_selections = n;
     } else {
         status = -1;
