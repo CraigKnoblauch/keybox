654bd32bb0d89ea042a52799c5d6a5f022ed3cb3
CraigKnoblauch <craigknoblauch@gmail.com>
2018-10-01 19:07:25 -0700

    proving lights can blink properly

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,16 @@
 #include <Matrix.cpp>
 
 int col[4] = {12, 11, 10, 9};
-int row[2] = {6, 5};
+int row[2] = {8, 7};
 
-Matrix matrix = Matrix( sizeof(row), row, sizeof(col), col );
+Matrix matrix = Matrix( 2, row, 4, col );
 
 void setup() {
     matrix.begin();
 }
 
 void loop() {
-    // Loop through all the LED's
-    for(int i = 0; i < sizeof(row)/4; i++) {
-        for(int j = 0; j < sizeof(col)/4; j++) {
-            matrix.setLED(i, j);
-            delay(500);
-        }
-    }
+    // Loop through all the LED's, in a circle
+    matrix.loopTest(1000);
+
 }
