57b46804e43141060da64f9a040b9b390225772e
CraigKnoblauch <craigknoblauch@gmail.com>
2018-10-01 21:37:27 -0700

    just proved my trim pot set up will work

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,25 @@ int row[2] = {8, 7};
 
 Matrix matrix = Matrix( 2, row, 4, col );
 
+#define CW 6
+#define WIPER 0
+
 void setup() {
     matrix.begin();
+
+    // Verify the trimpot is working
+    Serial.begin(9600);
+    pinMode(CW, OUTPUT);
+    pinMode(WIPER, INPUT);
+    digitalWrite(CW, HIGH);
 }
 
 void loop() {
-    // Loop through all the LED's, in a circle
-    matrix.loopTest(1000);
+    // Loop through all the LED's
+    // matrix.loopTest(1000);
+
+    int trim_val = analogRead(WIPER);
+    Serial.println(trim_val*0.004882);
+    delay(50);
 
 }
